6decec00e0ce6998f461aae5f79b6672798bef7d
Francesco Piero Paolicelli <piersoft2@gmail.com>
2024-02-16 15:56:35 +0100

    Update profiles.py

--- ckanext/dcatapit/dcat/profiles.py
+++ ckanext/dcatapit/dcat/profiles.py
@@ -874,11 +874,7 @@ class ItalianDCATAPProfile(RDFProfile):
             landing_page_uri = dataset_uri(dataset_dict)
             landing_page_uri=landing_page_uri.replace(PREF_LANDING,"https://dati.puglia.it")
             noaddsl=1 
-        if 'ispra_rm' in dataset_dict.get('holder_identifier'):
-            landing_page_uri = dataset_uri(dataset_dict)
-           #  landing_page_uri=landing_page_uri.replace(PREF_LANDING,"https://dati.puglia.it")
-            noaddsl=1             
-
+         
         if noaddsl==0:
            landing_page_uri += '/'
         self.g.add((dataset_ref, DCAT.landingPage, URIRef(landing_page_uri)))
