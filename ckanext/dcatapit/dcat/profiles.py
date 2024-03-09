64252f7dc9bb7c2cc934e5f8eb6f5b249964d935
Francesco Piero Paolicelli <piersoft2@gmail.com>
2024-03-09 14:18:05 +0100

    Update profiles.py

--- ckanext/dcatapit/dcat/profiles.py
+++ ckanext/dcatapit/dcat/profiles.py
@@ -764,6 +764,7 @@ class ItalianDCATAPProfile(RDFProfile):
             landing_page_uri=landing_page_uri.replace(PREF_LANDING,"https://dati-ustat.mur.gov.it/")
         if 'cmna' in dataset_dict.get('holder_identifier'):
             landing_page_uri=landing_page_uri.replace(PREF_LANDING,"https://dati.cittametropolitana.na.it/")
+            noaddsl=1
         if '00514490010' in dataset_dict.get('holder_identifier'):
             landing_page_uri=landing_page_uri.replace(PREF_LANDING,"http://aperto.comune.torino.it/")
         if 'r_lazio' in dataset_dict.get('holder_identifier'):
