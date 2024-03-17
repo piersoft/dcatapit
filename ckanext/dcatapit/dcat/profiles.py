d52443a0885d4d65bd21d7f9cb3a75684343ab62
Francesco Piero Paolicelli <piersoft2@gmail.com>
2024-03-17 22:23:51 +0100

    Update profiles.py

--- ckanext/dcatapit/dcat/profiles.py
+++ ckanext/dcatapit/dcat/profiles.py
@@ -772,6 +772,8 @@ class ItalianDCATAPProfile(RDFProfile):
             landing_page_uri = dataset_uri(dataset_dict)
             landing_page_uri=landing_page_uri.replace(PREF_LANDING,"http://dati.lazio.it/catalog/")
             noaddsl=1
+        if 'cciaan' in dataset_dict.get('holder_identifier'):
+            landing_page_uri = dataset_uri(dataset_dict)
         if 'r_basili' in dataset_dict.get('holder_identifier'):
             landing_page_uri = dataset_uri(dataset_dict)
             landing_page_uri=landing_page_uri.replace(PREF_LANDING,"https://dati.regione.basilicata.it/catalog/")
